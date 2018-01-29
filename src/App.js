4b3a6881cb3106a96ef7fd07d6a94406b98deffa
Dario L Garcia <dario.garcia@10pines.com>
2018-01-29 13:03:05 -0300

    Agrega el event-bus como dependencia

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Login from './login/Login';
 import VistaLogueado from './VistaLogueado';
+import EventBus from 'vertx3-eventbus-client';
+
+let eventBus = new EventBus('http://localhost:8080/eventbus');
+eventBus.onopen = function () {
+  eventBus.registerHandler('auction.1.price', function (error, message) {
+    console.log('EUR ' + JSON.parse(message.body));
+  });
+  eventBus.registerHandler('auction.1.bid', function (error, message) {
+    console.log('New offer: EUR ' + JSON.parse(message.body).price + '\n');
+  });
+};
 
 export default class App extends Component {
     constructor(props) {
