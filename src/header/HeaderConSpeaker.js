2382e45ff333bb02bd152b6746966b93791ee3ad
Dario L Garcia <dario.garcia@10pines.com>
2018-02-10 18:57:11 -0300

    Quitar proptypes

--- src/header/HeaderConSpeaker.js
+++ src/header/HeaderConSpeaker.js
@@ -1,13 +1,7 @@
 import React, {Component} from 'react';
 import {Label, Icon, Segment, Header} from 'semantic-ui-react';
-import PropTypes from 'prop-types';
 
 export default class HeaderConSpeaker extends Component {
-  static propTypes = {
-    speaker: PropTypes.shape({
-      nombre: PropTypes.string.isRequired
-    }).isRequired
-  };
 
   render() {
     return (
